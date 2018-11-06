f0fe1513e648a0116d73fd4a22932d8a41e82320
浪漫恋星空 <727378500@qq.com>
2018-11-06 10:11:25 +0800

    modification

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+inhibit_all_warnings!
+# platform :ios, '9.0'
+
+source 'https://gitee.com/jhcc/PrivatePods.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'TemplateProject' do
+
+pod 'CTMediator'
+
+end
---
+++ TemplateProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TemplateProject
+//
+//  Created by 浪漫恋星空 on 2018/11/6.
+//  Copyright © 2018 浪漫恋星空. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
